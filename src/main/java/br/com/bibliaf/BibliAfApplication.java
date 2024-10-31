5cd1f4fa94d6ed794d06c8f97a494d947c6849b1
RenanTGMF <renan_mariano20@hotmail.com>
2024-10-31 00:51:38 -0300

    init: initialize project with basic structure

---
+++ src/main/java/br/com/bibliaf/BibliAfApplication.java
@@ -0,0 +1,13 @@
+package br.com.bibliaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliAfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliAfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bibliaf/BibliAfApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bibliaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliAfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
